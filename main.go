208fe626153361768a7cf0aa4892250331752f45
Scott Bergler <commandinghands@gmail.com>
2019-05-21 07:24:08 -0700

    begin adding Product functionality to main().

--- main.go
+++ main.go
@@ -23,14 +23,23 @@ var psqlDatabaseConnectionString = fmt.Sprintf("host=%s port=%d user=%s password
 
 func main() {
 	models.InitDB(psqlDatabaseConnectionString)
+
 	http.HandleFunc("/customers", getCustomers)
 	http.HandleFunc("/customers/find", getCustomer)
 	http.HandleFunc("/customers/add", addCustomer)
 	http.HandleFunc("/customers/edit", editCustomer)
 	http.HandleFunc("/customers/delete", deleteCustomer)
+
+	http.HandleFunc("/products", getProducts)
+	http.HandleFunc("/products/find", getProduct)
+	// http.HandleFunc("/products/add", addProduct)
+	// http.HandleFunc("/products/edit", editProduct)
+	http.HandleFunc("/products/delete", deleteProduct)
+
 	http.ListenAndServe(":3000", nil)
 }
 
+// Customer functions
 func getCustomers(w http.ResponseWriter, r *http.Request) {
 	checkHTTPMethod(w, r, "GET")
 
@@ -149,6 +158,133 @@ func deleteCustomer(w http.ResponseWriter, r *http.Request) {
 	}
 
 	rowsAffected, err := models.DeleteCustomer(cstmrID)
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	marshalAndWriteJSON(w, rowsAffected)
+}
+
+// Product functions
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	checkHTTPMethod(w, r, "GET")
+
+	prdcts, err := models.AllProducts()
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	marshalAndWriteJSON(w, prdcts)
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	checkHTTPMethod(w, r, "GET")
+
+	id := r.FormValue("id")
+	if id == "" {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	productID, err := strconv.Atoi(id)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	prdct, err := models.GetProduct(productID)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	marshalAndWriteJSON(w, prdct)
+}
+
+// func addProduct(w http.ResponseWriter, r *http.Request) {
+// 	checkHTTPMethod(w, r, "POST")
+
+// 	name := r.FormValue("name")
+// 	productType := r.FormValue("type")
+// 	description := r.FormValue("descrition")
+// 	ingredients := r.FormValue("ingredients")
+// 	price := r.FormValue("price")
+// 	if name == "" || productType == "" || description == "" || ingredients == "" || price == "" {
+// 		http.Error(w, http.StatusText(400), 400)
+// 		return
+// 	}
+
+// 	record := models.Product{
+// 		Name:        name,
+// 		Type:        productType,
+// 		Description: description,
+// 		Ingredients: []ingredients,
+// 		Price:       price,
+// 	}
+
+// 	rowsAffected, err := models.AddProduct(record)
+// 	if err != nil {
+// 		http.Error(w, http.StatusText(500), 500)
+// 		return
+// 	}
+
+// 	marshalAndWriteJSON(w, rowsAffected)
+// }
+
+// func editProduct(w http.ResponseWriter, r *http.Request) {
+// 	checkHTTPMethod(w, r, "PUT")
+
+// 	id := r.FormValue("id")
+// 	name := r.FormValue("name")
+// 	productType := r.FormValue("productType")
+// 	description := r.FormValue("description")
+// 	ingredients := r.FormValue("ingredients")
+// 	price := r.FormValue("price")
+// 	if id == "" || name == "" || productType == "" || description == "" || ingredients == "" || price == "" {
+// 		http.Error(w, http.StatusText(400), 400)
+// 		return
+// 	}
+
+// 	prdctID, err := strconv.Atoi(id)
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	product := models.Product{
+// 		ID:          prdctID,
+// 		Name:        name,
+// 		Type:        productType,
+// 		Description: description,
+// 		Ingredients: ingredients,
+// 		Price:       price,
+// 	}
+
+// 	rowsAffected, err := models.EditProduct(product)
+// 	if err != nil {
+// 		http.Error(w, http.StatusText(500), 500)
+// 		return
+// 	}
+
+// 	marshalAndWriteJSON(w, rowsAffected)
+// }
+
+func deleteProduct(w http.ResponseWriter, r *http.Request) {
+	checkHTTPMethod(w, r, "DELETE")
+
+	id := r.FormValue("id")
+	if id == "" {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	prdctID, err := strconv.Atoi(id)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	rowsAffected, err := models.DeleteProduct(prdctID)
 	if err != nil {
 		http.Error(w, http.StatusText(500), 500)
 		return
